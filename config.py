6afb9223eefa38727828440952d62a91b99ff8d1
IM VETRI <69356705+ItsVetri@users.noreply.github.com>
2021-06-10 19:37:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "5363243"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "c111d35b9c60235e0c9b2c0bcbfbafb2" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "1861568051:AAE9SEsWoEC04p0WOQvrttDwNkjlrI_9qZs" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
